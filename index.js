86fea16ab6b2f20f8eea497be834f50343ca8345
Osvaldo Ambrosio <osvaldoambrosio@Osvaldos-MacBook-Pro.local>
2023-01-20 12:59:47 -0600

    Add all files to our repo

---
+++ index.js
@@ -0,0 +1,17 @@
+let username = 'osva'
+let age = 30
+let hasHobbies = false
+let points =[10, 20, 30]
+let user = {
+    name: 'ryan',
+    lastname: 'ray'
+}
+const PI = 3.1415
+
+ console.log(username)
+ console.log(age)
+ console.log(hasHobbies)
+ console.log(points)
+ console.log(user)
+ console.log(user.name)
+ console.log(PI)
